a9d3a5d5915ca9d9677eaea18257ec87fa64e739
Helal Uddin <helaluddinbd6320@gmail.com>
2025-12-16 07:40:54 +0300

    Update

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -2,16 +2,42 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Setting;
 use Illuminate\Database\Seeder;
 
 class SettingSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        Setting::firstOrCreate(
+            ['app_name' => 'Visa Office Chapai International'],
+            [
+                'office_phone' => '+880 1234 567890',
+                'office_phone2' => '+880 9876 543210',
+                'office_address' => 'Chapai Nawabganj, Rajshahi, Bangladesh',
+                'office_email' => 'info@visachapai.com',
+                'whatsapp_number' => '+880 1234 567890',
+                'facebook_url' => 'https://facebook.com/visachapai',
+                'instagram_url' => 'https://instagram.com/visachapai',
+                'primary_color' => '#3b82f6',
+                'secondary_color' => '#1e40af',
+                'tertiary_color' => '#10b981',
+                'currency' => 'BDT',
+                'currency_symbol' => '৳',
+                'timezone' => 'Asia/Dhaka',
+                'date_format' => 'd/m/Y',
+                'smtp_from_address' => 'noreply@visachapai.com',
+                'smtp_from_name' => 'Visa Office Chapai International',
+                'enable_email_marketing' => true,
+                'enable_sms_marketing' => true,
+                'sms_provider' => 'bulkbd',
+                'sms_sender_id' => 'VISACHAP',
+                'sms_unit_price' => 0.50,
+                'email_daily_limit' => 500,
+                'sms_daily_limit' => 100,
+                'invoice_prefix' => 'INV-',
+                'invoice_footer' => 'Thank you for your business!',
+            ]
+        );
     }
 }
